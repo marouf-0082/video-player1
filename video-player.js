07851b02a7acd396d737865ff67a42ce8d1c9c86
next1code <next1code@yahoo.com>
2025-03-07 15:19:31 +0430

    and make hide_controls function to hide

--- video-player.js
+++ video-player.js
@@ -5,3 +5,7 @@ const control_container = document.querySelector('.controls-main');
 function show_controls() {
   control_container.style.opacity = 1;
 }
+
+function hide_controls() {
+  control_container.style.opacity = 0;
+}
