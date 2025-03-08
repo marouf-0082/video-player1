f37cfa281fa1475ff3a31fe0b81f668fce0060e6
next1code <next1code@yahoo.com>
2025-03-08 12:02:09 +0430

    add some arrow key for down or up the volume and make a videoSound function

--- video-player.js
+++ video-player.js
@@ -1,7 +1,8 @@
 const control_container = document.querySelector('.controls-main');
 const video = document.querySelector('video');
 const play_btn = document.querySelector('.btn-play i');
-
+const volume_show = document.querySelector('.volume-show');
+const volume_btn_icon = document.querySelector('.valume-btn');
 
 function show_controls() {
   control_container.style.opacity = 1;
@@ -15,6 +16,7 @@ function play_video() {
   if (video.paused) {
     video.play();
     play_btn.classList.replace("fa-play", "fa-pause");
+    durationOfPlayVideo();
   } else {
     video.pause();
     play_btn.classList.replace('fa-pause', 'fa-play');
@@ -26,8 +28,10 @@ function seekButton(num) {
     video.currentTime += (num * 5);
     video.play();
     play_btn.classList.replace("fa-play", "fa-pause");
+    durationOfPlayVideo();
   } else {
     video.currentTime += (num * 5);
+    durationOfPlayVideo();
   }
 }
 
@@ -37,4 +41,32 @@ video.addEventListener('timeupdate', () => {
   document.querySelector('.progress').style.width = percent + '%';
 });
 }
-durationOfPlayVideo();
+
+document.onkeydown = (e) => {
+  switch(e.key) {
+    case 'ArrowUp': 
+      if (video.volume < 1) {
+        video.volume = (video.volume + 0.1).toFixed(1);
+        videoSound();
+      }
+      break;
+    case 'ArrowDown': 
+      if (video.volume > 0) {
+        video.volume = (video.volume - 0.1).toFixed(1);
+        videoSound();
+      }
+      break;
+  }
+}
+
+function videoSound() {
+  volume_show.textContent = (video.volume * 100) + '%';
+
+  if (volume_show.textContent >= 50+'%') {
+    volume_btn_icon.innerHTML = `<i class="fas fa-volume-up"></i>`;
+  } else if (volume_show.textContent > 0+'%') {
+    volume_btn_icon.innerHTML = `<i class="fas fa-volume-down"></i>`;
+  } else if (volume_show.textContent === 0+'%') {
+    volume_btn_icon.innerHTML = `<i class="fas fa-volume-mute"></i>`;
+  }
+}
